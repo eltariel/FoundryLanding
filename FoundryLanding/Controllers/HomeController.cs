123d06c285cd1b03175b6456f6c572bf9dce38ad
Eltariel <ellie@eltariel.com>
2020-06-09 17:59:22 +1000

    Boilerplate and minor changes

---
+++ FoundryLanding/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FoundryLanding.Models;
+
+namespace FoundryLanding.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            foreach (var (header, values) in HttpContext.Request.Headers)
+            {
+                _logger.LogInformation($"Header [{header}] = {string.Join(", ", values)}");
+            }
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
